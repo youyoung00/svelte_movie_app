b250a470779315f149f3134d416bc39d84450d74
shinhwan <you889@hanmail.net>
2022-10-23 22:22:17 +0900

    경로 와일드카드 추가

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,22 +2,43 @@
     import { link } from 'svelte-spa-router'
     import active from 'svelte-spa-router/active'
     import Logo from '~/components/Logo.svelte'
+
+    const menus = [
+        {
+            href: '/',
+            name: 'Search',
+            // path: '/'
+        },
+        {
+            href: '/movie/tt4520988',
+            name: 'Movie',
+            path: '/movie/*'
+        },
+        {
+            href: '/about',
+            name: 'About',
+            path: '/about'
+        }
+    ]
 </script>
 
 <header>
     <Logo />
-    <a 
-        use:link 
-        use:active
-        href="/">
-        Home
-    </a>
-    <a 
-        use:link 
-        use:active={'/about'}
-        href="/about">
-        About
-    </a>
+    <nav>
+        <ul>
+            {#each menus as {href, name, path} (name)}
+                <li>
+                    <a 
+                        use:link 
+                        use:active={path}
+                        {href}>
+                        {name}
+                    </a>
+                </li>
+            {/each}
+        </ul>
+    </nav>
+    
 </header>
 
 <style lang="scss">

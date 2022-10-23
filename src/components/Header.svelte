626b4376232f7a33343d63f20649acfcdfeddbed
shinhwan <you889@hanmail.net>
2022-10-23 18:23:56 +0900

    라우츠 링크와 하이라이팅

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,3 +1,27 @@
+
+<script>
+    import { link } from 'svelte-spa-router'
+    import active from 'svelte-spa-router/active'
+</script>
+
 <header>
     HEADER!
+    <a 
+        use:link 
+        use:active
+        href="/">
+        Home
+    </a>
+    <a 
+        use:link 
+        use:active={'/about'}
+        href="/about">
+        About
+    </a>
 </header>
+
+<style lang="scss">
+    header :global(a.active) {
+        color: red;
+    }
+</style>

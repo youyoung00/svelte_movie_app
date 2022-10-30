696c74244995e7a72f92565a21e4fab1d7c25b55
shinhwan <you889@hanmail.net>
2022-10-30 16:26:05 +0900

    vscode scss 스타일 에러 해결

---
+++ svelte.config.js
@@ -0,0 +1,23 @@
+
+const production = process.env.NODE_ENV === 'production'
+function babelOptions(){
+    return {
+        plugins: production 
+            ? ['transform-remove-console'] 
+            : []
+    }
+}
+
+module.export = {
+    preprocess: require('svelte-preprocess')({
+        scss: {
+            prependData: '@import "./src/scss/main.scss";'
+        },
+        postcss: {
+            plugins: [
+                require('autoprefixer')()
+            ]
+        },
+        babel: babelOptions()
+    })
+}

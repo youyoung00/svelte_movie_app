dcedaa36f06659c5b84362de900d24f76b19e65c
shinhwan <you889@hanmail.net>
2022-11-03 18:01:46 +0900

    응답에러 출력과 movie.js 리팩토링

--- src/components/MovieList.svelte
+++ src/components/MovieList.svelte
@@ -1,13 +1,18 @@
 <script>
-    import { movies, loading } from '~/store/movie'
-    import MovieCard from '~/components/MovieCard.svelte';
+    import { movies, loading, message } from '~/store/movie'
+    import MovieCard from '~/components/MovieCard.svelte'
     import Loader from '~/components/Loader.svelte'
 </script>
 
-<div class="movie-list">
+<div
+    class:no-result={!$movies.length}
+    class="movie-list">
     {#if $loading}
         <Loader />
     {/if}
+    <div class="message">
+        {$message}
+    </div>
     <div class="movies">
         {#each $movies as movie (movie.imdbID) }
             <MovieCard {movie}/>
@@ -22,6 +27,14 @@
         background-color: $color--area;
         border-radius: 4px;
         text-align: center;
+        &.no-result {
+            padding: 70px 0;
+        }
+        .message{
+            color: $color--primary;
+            font-size: 20px;
+            text-align: center;
+        }
         .movies{
             display: flex;
             flex-wrap: wrap;
--- src/store/movie.js
+++ src/store/movie.js
@@ -5,31 +5,43 @@ import { writable, get } from 'svelte/store'
 export const movies = writable([])
 export const loading = writable(false)
 export const theMovie = writable({})
+export const message = writable('Search for the movie title!')
 
 export async function searchMovies(payload) {
 
     if(get(loading)) return 
     loading.set(true)
 
-    const {title, type, year, number} = payload
-    const ONDB_API_KEY = '7035c60c'
-
-const res = await axios.get(`https://www.omdbapi.com/?apikey=${ONDB_API_KEY}&s=${title}&type=${type}&y=${year}`)
-    console.log(res)
-
-    const {Search, totalResults} = res.data
-    movies.set(Search)
+    let total = 0
+    
+    try {
+        const res = await _fetchMovie({
+            ...payload,
+            page: 1
+        })
+        const {Search, totalResults} = res.data
+        movies.set(Search)  
+        total = totalResults
+    } catch (msg) {
+        movies.set([])
+        message.set(msg)
+        loading.set(false)
+        return 
+    }
 
     // 검색결과 14 / 10 => 1.4 => 2페이지
     // 검색결과 7 / 0.7 => 1 => 1페이지
     // 검색결과 63 / 6.3 => 1.4 => 7페이지
-    const pageLength = Math.ceil(totalResults / 10)
+    const pageLength = Math.ceil(total / 10)
 
     if(pageLength > 1){
         for(let page = 2; page <= pageLength; page += 1){
             if(page > (number / 10)) break
-            const res = await axios.get(`https://www.omdbapi.com/?apikey=${ONDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)       
-            const {Search} = res.data
+            const res = await _fetchMovie({
+                ...payload,
+                page: page
+            }) 
+            const { Search } = res.data
             movies.update($movies => _unionBy($movies, Search, 'imdbID'))
         }
     }
@@ -40,10 +52,35 @@ const res = await axios.get(`https://www.omdbapi.com/?apikey=${ONDB_API_KEY}&s=$
 export async function searchMovieWithId(id){
     if(get(loading)) return
     loading.set(true)
-    const ONDB_API_KEY = '7035c60c'
-    const res = await axios.get(`https://www.omdbapi.com/?apikey=${ONDB_API_KEY}&i=${id}&plot=full`)
+    
+    const res = await _fetchMovie({
+        id: id,
+    })
     console.log(res)
 
     theMovie.set(res.data)
     loading.set(false)
+}
+
+function _fetchMovie(payload){
+    const { title, type, year, page, id } = payload
+    const OMDB_API_KEY = '7035c60c'
+
+    const url = id 
+        ? `https://www.omdbapi.com/?apikey=${ONDB_API_KEY}&i=${id}&plot=full`
+        : `https://www.omdbapi.com/?apikey=${ONDB_API_KEY}&s=${title}&type=${type}&y=${year}`
+
+    return new Promise( async (resolve, reject) => {
+        try{
+            const res = await axios.get(url)
+            console.log(res.data)
+            if(res.data.Error){
+                reject(res.error.message)
+            }
+            resolve(res)
+        } catch (error) {
+            console.log(error.response.status)
+            reject(error.message)
+        }
+    })
 }
